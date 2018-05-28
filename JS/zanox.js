10058c8d69d83aff5c7f65401c5bfe411b52e185
Paula <paulacsalmeida@gmail.com>
2018-05-28 18:35:29 -0300

    jhf

---
+++ JS/zanox.js
@@ -0,0 +1,87 @@
+Old
+
+
+<!-- Start of Checkout Page Mastertag Code: -->
+<div class="zx_61C3F2236749DAE096DC zx_mediaslot">
+    <script type="text/javascript">
+
+        var zx_product = [], zx_products = [];
+        for (var x = 0 ; x < dataLayer.length ; x++){
+            if(dataLayer[x].transactionProducts != undefined){
+                for (var i = 0 ; i < dataLayer[x].transactionProducts.length ; i++){
+                    var content = {
+                        'identifier': dataLayer[x].transactionProducts[i].id,
+                        'amount':dataLayer[x].transactionProducts[i].price,
+                        'currency':'BRL',
+                        'quantity': dataLayer[x].transactionProducts[i].quantity
+                    };
+
+                    zx_product.push(content);
+                  	zx_products.push(content);
+                }
+                x = 100;
+            }
+        }
+
+        window._zx = window._zx || [];
+        window._zx.push({"id":"61C3F2236749DAE096DC"});
+        zx_product;
+      	zx_products;
+        (function(d) {
+            var s = d.createElement("script"); s.async = true;
+            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//static.zanox.com/scripts/zanox.js";
+            var a = d.getElementsByTagName("script")[0]; a.parentNode.insertBefore(s, a);
+        }(document));
+    </script>
+</div>
+<!-- End of Checkout Page Mastertag Code -->
+
+
+
+New
+
+<!-- Start of Checkout Page Mastertag Code: -->
+<div class="zx_61C3F2236749DAE096DC zx_mediaslot">
+    <script type="text/javascript">
+
+        var zx_product = [];
+        var zx_total_currency = [];
+        var zx_total_amount = [];
+        var zx_transaction = [];
+        var zx_identifier = [];
+
+        for (var x = 0 ; x < dataLayer.length ; x++){
+            if(dataLayer[x].transactionProducts != undefined){
+                for (var i = 0 ; i < dataLayer[x].transactionProducts.length ; i++){
+                    var content = {
+                        'identifier': dataLayer[x].transactionProducts[i].id,
+                        'amount':dataLayer[x].transactionProducts[i].price,
+                        'currency':'BRL',
+                        'quantity': dataLayer[x].transactionProducts[i].quantity
+                    };
+
+                    zx_product.push(content);
+                    zx_total_currency.push(content);
+                    zx_total_amount.push(content);
+                    zx_transaction.push(content);
+                    zx_identifier.push(content);
+                }
+                x = 100;
+            }
+        }
+
+        window._zx = window._zx || [];
+        window._zx.push({"id":"61C3F2236749DAE096DC"});
+        zx_product;
+        zx_total_currency;
+        zx_total_amount;
+        zx_transaction;
+        zx_identifier;
+        (function(d) {
+            var s = d.createElement("script"); s.async = true;
+            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//static.zanox.com/scripts/zanox.js";
+            var a = d.getElementsByTagName("script")[0]; a.parentNode.insertBefore(s, a);
+        }(document));
+    </script>
+</div>
+<!-- End of Checkout Page Mastertag Code -->
